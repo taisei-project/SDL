c021268b1ebbc0bb45c27454cbd3baa3bc602521
Andrei Alexeyev <akari@taisei-project.org>
2024-08-29 02:25:43 +0200

    render_gpu: Banish it to the shadow realm

--- src/render/SDL_render.c
+++ src/render/SDL_render.c
@@ -106,9 +106,6 @@ this should probably be removed at some point in the future.  --ryan. */
 
 #ifndef SDL_RENDER_DISABLED
 static const SDL_RenderDriver *render_drivers[] = {
-#if SDL_VIDEO_RENDER_GPU
-    &GPU_RenderDriver,
-#endif
 #if SDL_VIDEO_RENDER_D3D11
     &D3D11_RenderDriver,
 #endif
@@ -138,6 +135,9 @@ static const SDL_RenderDriver *render_drivers[] = {
 #endif
 #if SDL_VIDEO_RENDER_VULKAN
     &VULKAN_RenderDriver,
+#endif
+#if SDL_VIDEO_RENDER_GPU
+    &GPU_RenderDriver,
 #endif
 #if SDL_VIDEO_RENDER_SW
     &SW_RenderDriver
